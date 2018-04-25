1c2b3637f11ace3301707d4d37b1714babdfae8e
Michael Salama <MichaelSalama@users.noreply.github.com>
2018-04-25 16:48:01 +0200

    Random map generation
    Random Generation for the map with prefabs of obstacles and player movement, Destroy and snapping isn't handeled

---
+++ Assets/Scripts/PlayerMoving.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoving : MonoBehaviour
+{
+
+    private CharacterController controller;
+    private Vector3 moveVector;
+    public float speed = 5.0f;
+   
+   
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+
+        moveVector = Vector3.zero;
+
+        moveVector.x = Input.GetAxisRaw("Horizontal") * speed; //  for Left and Right movement
+
+        moveVector.z = speed;                                     // for ongoing speed
+        controller.Move(moveVector * Time.deltaTime);
+    }
+}
