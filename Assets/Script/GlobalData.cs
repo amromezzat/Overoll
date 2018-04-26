14a7afe2276792323ab30b9fd6745d375d42ab66
AUEzzat <amr.m.ezzat@gmail.com>
2018-04-26 06:41:40 +0200

    testing flock behavior

---
+++ Assets/Script/GlobalData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GlobalData
+{
+    public static GameObject Leader;
+    public static int LaneWidth;
+    public static List<GameObject> workers;
+    public static List<Rigidbody> workersRb;
+}
---
+++ Assets/Script/PositionWorker.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionWorker : MonoBehaviour
+{
+    Rigidbody rb;
+    int maxForce = 200;
+    int maxVel = 5;
+    float neighborDist = 2f;
+    Vector3 steerForce;
+
+    // Use this for initialization
+    void Start()
+    {
+        steerForce = Vector3.zero;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        steerForce = FollowLeader() + KeepDistance();
+    }
+
+    Vector3 FollowLeader()
+    {
+        //Vector3 LeaderFutureLocation = GlobalData.Leader.GetComponent<Rigidbody>().velocity.normalized * 100 + GlobalData.Leader.transform.position;
+        Vector3 desiredVelocity = GlobalData.Leader.transform.position - transform.position;
+        desiredVelocity = desiredVelocity.normalized * maxVel;
+        Vector3 steerToLeader = desiredVelocity - rb.velocity;
+        steerToLeader = steerToLeader.normalized * maxForce;
+        float decelerate = GetDeceleration(desiredVelocity.magnitude, steerToLeader.magnitude);
+        steerToLeader *= decelerate * Time.deltaTime;
+        if (GlobalData.Leader.transform.position.z - GlobalData.LaneWidth < transform.position.z
+            && GlobalData.Leader.transform.position.z + GlobalData.LaneWidth > transform.position.z)
+        {
+            steerToLeader.z = 0;
+        }
+        return steerToLeader;
+    }
+
+    Vector3 KeepDistance()
+    {
+        Vector3 averageDesiredVelocity = Vector3.zero;
+        int count = 0;
+        for (int i = 0; i < GlobalData.workers.Count; i++)
+        {
+            if (GlobalData.workers[i].GetInstanceID() != GetInstanceID())
+            {
+                Vector3 averageDist = -transform.position + GlobalData.workers[i].transform.position;
+                if (averageDist.magnitude <= neighborDist)
+                {
+                    averageDesiredVelocity += averageDist;
+                    count++;
+                }
+            }
+        }
+        if (count > 0)
+        {
+            averageDesiredVelocity /= count;
+            averageDesiredVelocity = averageDesiredVelocity.normalized * maxVel;
+            Vector3 SteerToKeepDist = averageDesiredVelocity - rb.velocity;
+            SteerToKeepDist = -SteerToKeepDist.normalized * maxForce / 40;
+            //float decelerate = GetDeceleration(SteerToKeepDist.magnitude, SteerToKeepDist.magnitude);
+            //steerToLeader *= decelerate * Time.deltaTime;
+            return SteerToKeepDist;
+        }
+        return Vector3.zero;
+    }
+
+    private void FixedUpdate()
+    {
+        //rb.angularVelocity = Vector3.zero;
+        rb.velocity = steerForce;
+        //rb.AddForce(steerForce, ForceMode.VelocityChange);
+    }
+
+    //decelerate before hitting the leader
+    public static float GetDeceleration(float dist, float speed)
+    {
+        if (dist < 0.5)
+        {
+            return 0.1f / speed;
+        }
+        else if (dist < 2)
+        {
+            return dist / speed;
+        }
+
+        return 1;
+    }
+}
