b0082722e48a539d3c1a4ef828bfa3db7f14f494
tasnim <tassneemm0@gmail.com>
2018-04-28 16:43:54 +0200

    Coin system
    finished the scripted but still some errors to be fixed tomorrow isa

---
+++ Assets/Money System/script of mony system/Currency.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Curency : MonoBehaviour
+{
+	public int gold;
+	GameObject CurrencyUi;
+
+	// Use this for initialization
+	void Start () {
+		CurrencyUi = GameObject.Find ("currency");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CurrencyUi.GetComponent<Text>().text = gold.ToString(); 
+
+		if(gold < 0) //so that we don't have nof gold with -Ve 
+		{
+			gold = 0;
+
+		}
+	}
+}
