0598edf73353681d199a5ad7cd7f8f2af034bd17
tasnim <tassneemm0@gmail.com>
2018-04-30 19:15:20 +0200

    input manager
    still

---
+++ Assets/Tasnim/scripts/swiptest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class swiptest : MonoBehaviour {
+    public float maxTime;
+    public float minSwipDis;
+
+    float startTime;
+    float endTime;
+
+    Vector3 startPos;
+    Vector3 endpos;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
