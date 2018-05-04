7c1edf9b63e628e633ee474acafdfadd587c0b40
Michael Salama <MichaelSalama@users.noreply.github.com>
2018-05-04 23:26:17 +0200

    TIles Generation
    Apparently everthing is working, But there is a minor bug that I'm looking for.
    No scriptableObject used now as Pooling always use MonoBehaviour

---
+++ Assets/Michael/Script/PlayerManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+
+    public Player_SO playerManager;
+
+    public void Awake()
+    {
+    
+        playerManager.isalive = true;
+    }  
+}
---
+++ Assets/Michael/Script/Player_SO.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Game Player_SO", menuName = "Michael/Player")]
+public class Player_SO : ScriptableObject
+{
+   public bool isalive;
+}
