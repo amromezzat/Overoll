de2cd7f59072159951fb943b5a6d1d9ba039d9f6
Michael Salama <MichaelSalama@users.noreply.github.com>
2018-05-06 19:02:38 +0200

    pooling for tiles
    more fix fix fix

---
+++ Assets/Michael/Script/Pool/Returner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Returner : MonoBehaviour
+{
+    public TileConfig tc;
+    public ObjectPooler op;
+
+    private void Start()
+    {
+        op = GetComponentInParent<ObjectPooler>();
+    }
+
+    private void Update()
+    {
+        if (gameObject.transform.position.z < -5)
+        {
+            op.ReturnObjectToPool(this.gameObject);
+        }
+    }
+
+}
---
+++ Assets/Michael/Script/ScriptableObject/TileConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu (menuName ="Config/Tile Config")]
+public class TileConfig : ScriptableObject
+{
+    public float tileSpeed;
+    public float tileSize;
+    public float laneWidth;
+}
