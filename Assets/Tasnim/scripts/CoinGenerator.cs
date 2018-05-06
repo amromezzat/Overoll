e9776ddda005e1ddd26070b260491d453c7459be
tasnim <tassneemm0@gmail.com>
2018-05-06 16:38:42 +0200

    coin pool
    unfinished coingenerator

--- Assets/Tasnim/scripts/CoinGenerator.cs
+++ Assets/Tasnim/scripts/CoinGenerator.cs
@@ -3,7 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CoinGenerator : MonoBehaviour {
-    public ObjectPooler coinpool;
-    public float disbetweencoins;
+    coinbool coinpoolscript;
+    public Vector3 disbetweencoins;
 
+    public int numberfollowedcoins;
+
+    void Start()
+    {
+        coinpoolscript = this.GetComponent<coinbool>();
+
+    }
+
+
+    void Generate()
+    {
+        for (int i = 0; i < numberfollowedcoins; i++)
+        {
+            GameObject generatedcoin = coinpoolscript.GetCoinFromPool();
+            //call get coin from the coin pool
+
+            generatedcoin.transform.position
+        }
+        
+    }
 }
