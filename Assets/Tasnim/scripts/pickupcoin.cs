6e573beb9edce941818db8e33014b0855f07d858
tasnim <tassneemm0@gmail.com>
2018-05-06 11:19:14 +0200

    pick coin

---
+++ Assets/Tasnim/scripts/pickupcoin.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickupcoin : MonoBehaviour {
+
+    public int CoinCount;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("collided");
+        if (other.gameObject.name == "Player")
+        {
+            CoinCount += 1;
+            Debug.Log(CoinCount);
+        }
+    }
+}
