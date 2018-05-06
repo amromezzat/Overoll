e698b500a164204e448d48e6d324fb7f0bd97599
tasnim <tassneemm0@gmail.com>
2018-05-06 16:38:42 +0200

    coin pool
    unfinished coingenerator

---
+++ Assets/Tasnim/scripts/coinbool.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coinbool : MonoBehaviour {
+
+    public GameObject prefab;
+    public int coinpoolSize;
+    public List<GameObject> CoinPool;
+
+    // we need random position to start in random lanes 
+
+	// Use this for initialization
+	void Start () {
+
+        for (int i = 0; i < coinpoolSize; i++)
+        {
+            AddToCoinPool();
+        }
+		
+	}
+    //------------------------------------------------
+    public GameObject GetCoinFromPool()
+    {
+        if (CoinPool.Count == 0)
+        {
+            AddToCoinPool();
+        }
+        GameObject Element = CoinPool[1];
+        Element.SetActive(true);
+       
+        CoinPool.Remove(Element);
+        return Element;
+    } 
+    //----------------------------------------------
+    public void ReturnCoinToPool(GameObject ObjToReturn)
+    {
+        ObjToReturn.SetActive(false);
+        CoinPool.Add(ObjToReturn);
+    }
+    //----------------------------------------------
+    void AddToCoinPool()
+    {
+        var obj = Instantiate(prefab);
+        obj.SetActive(false);
+        CoinPool.Add(obj);
+
+    }
+	
+	
+}
