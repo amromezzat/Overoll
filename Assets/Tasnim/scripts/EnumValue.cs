a4b5ca6c240bb292428f91c06231be9c53c2e2db
tasnim <tassneemm0@gmail.com>
2018-05-09 19:20:44 +0200

    obstacles os
    ha bush ya michael :P

---
+++ Assets/Tasnim/scripts/EnumValue.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName ="ObsticaleTypeEnumValue", menuName="Obsticals/EnumValueType" ,order = 9)]
+public class EnumValue : ScriptableObject {
+
+}
---
+++ Assets/Tasnim/scripts/ObstacleSystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class ObstacleSystem : ScriptableObject {
+
+ public    List<EnumValue> Obstacles;
+
+
+  public  Dictionary<string, EnumValue> ListOfObstacles= new Dictionary<string, EnumValue>();
+
+   void OnEnable()
+    {
+        for (int i = 0; i < Obstacles.Count; i++)
+        {
+            ListOfObstacles.Add(Obstacles[i].name, Obstacles[i]);
+        }
+       
+
+    }
+
+
+}
+
+
