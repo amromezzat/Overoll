8c3b36f64bef274d8d3e129ffc2f11fb1ad9c214
Michael Salama <MichaelSalama@users.noreply.github.com>
2018-05-11 22:10:20 +0200

    pattern custom editor attempt
    pattern custom editior attempt -- not counted --

---
+++ Assets/Michael/Editor/PatternEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+//[CustomEditor(typeof(Pattern))]
+
+[CustomEditor(typeof(CoinPool))]
+public class PatternEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        GUILayout.Button("hamada");
+    }
+}
---
+++ Assets/Michael/Scripts/PatternSO.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pattern : ScriptableObject
+{
+    public int difficulty;
+    public float length;
+    public float sectionLength;
+   // public List<ObjectSO>;
+}
