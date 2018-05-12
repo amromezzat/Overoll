1af78d7c447fbaff834a263403c3b32ef09f8163
Michael Salama <MichaelSalama@users.noreply.github.com>
2018-05-12 22:01:28 +0200

    primary pattern custom editor
    function integration needed

--- Assets/Michael/Editor/PatternEditor.cs
+++ Assets/Michael/Editor/PatternEditor.cs
@@ -3,15 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-//[CustomEditor(typeof(Pattern))]
+/// <summary>
+/// This script is used to modify obstacles in patterns segement by segment
+/// </summary>
 
-[CustomEditor(typeof(CoinPool))]
+//[CustomEditor(typeof(Pattern))]
+//[CustomEditor(typeof(CoinGenerator))]
 public class PatternEditor : Editor
 {
+    public int noOfSegments;
 
     public override void OnInspectorGUI()
     {
-        DrawDefaultInspector();
-        GUILayout.Button("hamada");
+        // call some function here to genereate <noOfSegments> segment(s)
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("far left lane"))
+        {
+            // call the functuin of far left lane instantiation
+        }
+
+        if (GUILayout.Button("left lane"))
+        {
+            // call the functuin of left lane instantiation
+
+        }
+
+        if (GUILayout.Button("Middle Lane"))
+        {
+            // call the functuin of middle instantiation
+
+        }
+
+        if (GUILayout.Button("Right Lane"))
+        {
+            // call the functuin of Right lane instantiation
+
+        }
+
+        if (GUILayout.Button("far Righy Lane"))
+        {
+            // call the functuin of far Right lane instantiation
+
+        }
+
+        GUILayout.EndHorizontal();
     }
 }
