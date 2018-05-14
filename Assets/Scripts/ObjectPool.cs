cfdb5b503c570a71674c542c08f116c3708e8d2e
AUEzzat <amr.m.ezzat@gmail.com>
2018-05-14 17:29:05 +0200

    tile type

--- Assets/Scripts/ObjectPool.cs
+++ Assets/Scripts/ObjectPool.cs
@@ -2,12 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectPool : MonoBehaviour {
 public class ObjectPool : MonoBehaviour
 {
-
     public static ObjectPool instance;
-    public Dictionary<EnumValue, PrefabCount> prefabsDict;
     public PoolableDatabase pd;
 
     Dictionary<EnumValue, Queue<GameObject>> poolDict;
@@ -18,23 +15,17 @@ public class ObjectPool : MonoBehaviour
     }
 
     // Use this for initialization
-    void Start () {
-		foreach(EnumValue pcName in prefabsDict.Keys)
     void Start()
     {
         foreach (EnumValue pcType in pd.Keys)
         {
-            Queue < GameObject > instancesQueue = new Queue<GameObject>(prefabsDict[pcName].count);
-            for(int i = 0; i < prefabsDict[pcName].count; i++)
             Queue<GameObject> instancesQueue = new Queue<GameObject>(pd[pcType].count);
             for (int i = 0; i < pd[pcType].count; i++)
             {
-                GameObject newGameObj = Instantiate(prefabsDict[pcName].prefab, prefabsDict[pcName].parent.transform);
                 GameObject newGameObj = Instantiate(pd[pcType].prefab, pd[pcType].parent.transform);
                 newGameObj.SetActive(false);
                 instancesQueue.Enqueue(newGameObj);
             }
-            poolDict[pcName] = instancesQueue;
             poolDict[pcType] = instancesQueue;
         }
     }
@@ -50,7 +41,6 @@ public class ObjectPool : MonoBehaviour
                 pooledObj.SetActive(true);
                 return pooledObj;
             }
-            GameObject newGameObj = Instantiate(prefabsDict[instType].prefab, prefabsDict[instType].parent.transform);
             GameObject newGameObj = Instantiate(pd[instType].prefab, pd[instType].parent.transform);
             return newGameObj;
         }
@@ -64,7 +54,6 @@ public class ObjectPool : MonoBehaviour
         {
             Queue<GameObject> instQueue = poolDict[instType];
             inst.SetActive(false);
-            inst.transform.position = prefabsDict[instType].parent.transform.position;
             inst.transform.position = pd[instType].parent.transform.position;
             instQueue.Enqueue(inst);
         }
