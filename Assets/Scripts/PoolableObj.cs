156bd15eea990fdf73ab66500118382b90814bb7
AUEzzat <amr.m.ezzat@gmail.com>
2018-05-14 17:23:27 +0200

    pool editor and poolable object

--- Assets/Scripts/PoolableObj.cs
+++ Assets/Scripts/PoolableObj.cs
@@ -4,13 +4,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public struct PrefabCount {
+public struct PoolableObj {
 
     public int count;
     public GameObject prefab;
     public GameObject parent;
 
-    PrefabCount(int _count, GameObject _prefab, GameObject _parent)
+    PoolableObj(int _count, GameObject _prefab, GameObject _parent, EnumValue _type)
     {
         count = _count;
         prefab = _prefab;
---
+++ Assets/Scripts/ScriptableObjects/PoolableDatabase.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Lanes", menuName = "Config/Pool/PoolDatabase")]
+public class PoolableDatabase : ScriptableObject
+{
+
+    public Dictionary<EnumValue, PoolableObj> prefabsDict = new Dictionary<EnumValue, PoolableObj>();
+    int count = 0;
+    List<EnumValue> keys;
+
+    public PoolableObj this[EnumValue instType]
+    {
+        get
+        {
+            if (prefabsDict.ContainsKey(instType))
+            {
+                return prefabsDict[instType];
+            }
+            return new PoolableObj();
+        }
+    }
+
+    public EnumValue this[int index]
+    {
+        get
+        {
+            return Keys[index];
+        }
+    }
+
+    public int Count
+    {
+        get
+        {
+            count = prefabsDict.Count;
+            return count;
+        }
+    }
+
+    public List<EnumValue> Keys
+    {
+        get
+        {
+            List<EnumValue> keysList = new List<EnumValue>(Count);
+            foreach (EnumValue key in keys)
+            {
+                keysList.Add(key);
+            }
+            return keysList;
+        }
+    }
+}
