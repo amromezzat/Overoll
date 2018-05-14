2d25d82d81f3debbdfc27f3e323c32d46d1a0937
tasnim <tassneemm0@gmail.com>
2018-05-14 19:17:32 +0200

    Pattern Data Base

---
+++ Assets/Tasnim/scripts/PatternDB.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class PatternDB : ScriptableObject {
+
+
+    public List<List<PatternSO>> PatternDataBase = new List<List<PatternSO>>();
+    public List<TileType> segment = new List<TileType>(5); 
+
+
+    void onEnable()
+    {
+        List<PatternSO> psoList = new List<PatternSO>(); 
+         //  psoList.Add(AssetDatabase.FindAssets("t:" + typeof(PatternSO).Name));
+    } 
+   
+}
