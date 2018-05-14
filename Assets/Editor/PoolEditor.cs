0bdb43cb6b60953d7679e76befb34e240ce31a4c
AUEzzat <amr.m.ezzat@gmail.com>
2018-05-14 17:23:27 +0200

    pool editor and poolable object

--- Assets/Editor/PoolEditor.cs
+++ Assets/Editor/PoolEditor.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 using UnityEditor;
 
 [CustomEditor(typeof(ObjectPool))]
+[CustomEditor(typeof(PoolableDatabase))]
 public class PoolEditor : Editor
 {
 
     ObjectPool objPool;
 
+    PoolableDatabase poolableDB;
     private void OnEnable()
     {
         objPool = (ObjectPool)target;
+        poolableDB = (PoolableDatabase)target;
     }
 
     public override void OnInspectorGUI()
@@ -20,10 +23,25 @@ public class PoolEditor : Editor
 
         int i = 1;
         foreach (KeyValuePair<EnumValue, PrefabCount> entry in ObjectPool.instance.prefabsDict)
+
+
+        //List<EnumValue> keys = poolableDB.Keys;
+        GUILayout.Label(poolableDB.Count.ToString());
+        for(int i=0;i< poolableDB.Count; i++)
         {
+            EditorGUILayout.BeginHorizontal();
             GUILayout.Label(i.ToString());
-            //EditorGUILayout.TextArea(ObjectPool.instance.prefabsDict.Values);
+            EditorGUILayout.LabelField(poolableDB[i].ToString());
+            EditorGUILayout.EndHorizontal();
         }
+
+        if (GUILayout.Button("Add"))
+        {
+            EnumValue ss = (EnumValue)ScriptableObject.CreateInstance("EnumValue");
+            ss.name = "sss";
+            poolableDB.prefabsDict.Add(ss, new PoolableObj());
+        }
+
 
 
     }
--- Assets/Scripts/ObjectPool.cs
+++ Assets/Scripts/ObjectPool.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectPool : MonoBehaviour {
+public class ObjectPool : MonoBehaviour
+{
 
     public static ObjectPool instance;
     public Dictionary<EnumValue, PrefabCount> prefabsDict;
+    public PoolableDatabase pd;
 
     Dictionary<EnumValue, Queue<GameObject>> poolDict;
 
@@ -17,17 +20,24 @@ public class ObjectPool : MonoBehaviour {
     // Use this for initialization
     void Start () {
 		foreach(EnumValue pcName in prefabsDict.Keys)
+    void Start()
+    {
+        foreach (EnumValue pcType in pd.Keys)
         {
             Queue < GameObject > instancesQueue = new Queue<GameObject>(prefabsDict[pcName].count);
             for(int i = 0; i < prefabsDict[pcName].count; i++)
+            Queue<GameObject> instancesQueue = new Queue<GameObject>(pd[pcType].count);
+            for (int i = 0; i < pd[pcType].count; i++)
             {
                 GameObject newGameObj = Instantiate(prefabsDict[pcName].prefab, prefabsDict[pcName].parent.transform);
+                GameObject newGameObj = Instantiate(pd[pcType].prefab, pd[pcType].parent.transform);
                 newGameObj.SetActive(false);
                 instancesQueue.Enqueue(newGameObj);
             }
             poolDict[pcName] = instancesQueue;
+            poolDict[pcType] = instancesQueue;
         }
-	}
+    }
 
     public GameObject GetFromPool(EnumValue instType)
     {
@@ -41,6 +51,7 @@ public class ObjectPool : MonoBehaviour {
                 return pooledObj;
             }
             GameObject newGameObj = Instantiate(prefabsDict[instType].prefab, prefabsDict[instType].parent.transform);
+            GameObject newGameObj = Instantiate(pd[instType].prefab, pd[instType].parent.transform);
             return newGameObj;
         }
         Debug.LogError("Instance is invalid");
@@ -54,6 +65,7 @@ public class ObjectPool : MonoBehaviour {
             Queue<GameObject> instQueue = poolDict[instType];
             inst.SetActive(false);
             inst.transform.position = prefabsDict[instType].parent.transform.position;
+            inst.transform.position = pd[instType].parent.transform.position;
             instQueue.Enqueue(inst);
         }
         Debug.LogError("Instance is invalid");
