713fc3e3542e04acd90ea011a5c6f063b2810dad
tasnim <tassneemm0@gmail.com>
2018-05-14 17:28:09 +0200

    organise

---
+++ Assets/Tasnim/scripts/TileGeneration.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileGeneration : MonoBehaviour {
+
+    public Lanes lanes;
+    ObjectPool PoolOfObstacles;
+ 
+
+
+	// Use this for initialization
+	void Start () {
+        PoolOfObstacles = GetComponent<ObjectPool>();
+        
+     
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+     void TileGenerator(Segment segment)
+    {
+        // i represent thelaneNumber
+        for (int i = 0; i < 5; i++)
+        {
+            if (!segment[i])
+            {
+                continue;
+
+            }
+
+            var obj = PoolOfObstacles.GetFromPool(segment[i]);
+            Vector3 objpos = obj.transform.position;
+             objpos.x = lanes[i].laneCenter;
+        }
+        
+
+    }
+
+
+  //  void RetunTileToPool()
+    //{
+
+    //}
+}
