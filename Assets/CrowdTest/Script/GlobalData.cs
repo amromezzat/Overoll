788876a99bf504cabd6f455dcb154a91e5637ee4
tasnim <tassneemm0@gmail.com>
2018-05-15 19:00:01 +0200

    workers models restored

---
+++ Assets/CrowdTest/Script/GlobalData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//a static class for accessible data across all monoscripts
+//pass data between different game managers
+public static class GlobalData
+{
+    //*****************************************************
+    //scene and leader shared data like lane width
+    //*****************************************************
+    //*****************************************************
+}
---
+++ Assets/CrowdTest/Script/MoveObsTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObsTest : MonoBehaviour {
+
+    public int speed = 5;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody>().velocity += Vector3.back * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.z < -20)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, 20);
+        }
+	}
+}
