de34312948777cc366513d86f69eeb806a535a75
tasnim <tassneemm0@gmail.com>
2018-05-16 12:01:51 +0200

    Edit in segment Class

---
+++ Assets/Tasnim/Editor/PatternDataBaseEditor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(PatternDB))]
+public class PatternDataBaseEditor : Editor {
+
+
+
+
+
+    public PatternDB PatternDataBase;
+    //Variables to Edit 
+    public PatternSO Difficulty;
+    public PatternSO Lenght;
+    //----------------------
+    public Segment segment;
+
+
+
+
+
+}
--- Assets/Tasnim/scripts/Segment.cs
+++ Assets/Tasnim/scripts/Segment.cs
@@ -8,19 +8,78 @@ using UnityEngine;
 
 public class Segment 
 {
-     public List<TileType> segment= new List<TileType>(5);
-    
-    public TileType this [int i]
+  //   public List<TileType> segment= new List<TileType>(5);
+
+    TileType Lane1;
+    TileType Lane2;
+    TileType Lane3;
+    TileType Lane4;
+    TileType Lane5;
+
+    public TileType this [int i ]
     {
 
         get
         {
-            return segment[i];
+            switch (i)
+            {
+                case 1:
+                    return Lane1;
+                    
+
+                case 2:
+                    return Lane2;
+                   
+
+                case 3:
+                    return Lane3;
+                   
+
+                case 4:
+                    return Lane4;
+                   
+                case 5:
+                    return Lane5;
+                    
+                default:
+                    return null;
+
+            }
+
+            
         }
 
         set
         {
-            segment[i] = value;
+            switch (i)
+            {
+                case 1:
+                    Lane1 = value;
+                    break;
+
+                case 2:
+                    Lane2 = value;
+                    break;
+
+                case 3:
+                    Lane3 = value;
+                    break;
+
+                case 4:
+                    Lane4 = value;
+                    break;
+                case 5:
+                    Lane5 = value;
+                    break;
+
+                //default:
+                //    Lane1 = null;
+                //    Lane2 = null;
+                //    Lane3 = null;
+                //    Lane4 = null;
+                //    Lane5 = null;
+            }
+          
         }
     }
 }
