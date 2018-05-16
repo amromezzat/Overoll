f42a91893feb6875dc63894746eaa448dc9d28b2
AUEzzat <amr.m.ezzat@gmail.com>
2018-05-16 04:37:33 +0200

    poolable object and pool editor

--- Assets/Scripts/PoolableObj.cs
+++ Assets/Scripts/PoolableObj.cs
@@ -3,15 +3,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-public struct PoolableObj {
-
+[Serializable]
+public class PoolableObj {
+    public TileType type;
     public int count;
     public GameObject prefab;
     public GameObject parent;
 
-    PoolableObj(int _count, GameObject _prefab, GameObject _parent)
+    public string Name
     {
+        get
+        {
+            return type.name;
+        }
+    }
+
+    public PoolableObj(TileType _type, int _count, GameObject _prefab, GameObject _parent)
+    {
+        type = _type;
         count = _count;
         prefab = _prefab;
         parent = _parent;
