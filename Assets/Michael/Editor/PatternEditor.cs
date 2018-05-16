b4686cd6cb3165f6b7ce950318409919ae78bf91
Michael Salama <MichaelSalama@users.noreply.github.com>
2018-05-16 13:21:54 +0200

    minor bug fixes

--- Assets/Michael/Editor/PatternEditor.cs
+++ Assets/Michael/Editor/PatternEditor.cs
@@ -10,24 +10,63 @@ using UnityEditor;
 [CustomEditor(typeof(PatternSO))]
 public class PatternEditor : Editor
 {
+    int segmentNum;
+    PatternSO currentInstance;
+    void OnEnable()
+    {
+        currentInstance = target as PatternSO;
+    }
 
     public override void OnInspectorGUI()
     {
         DrawDefaultInspector();
-
-        GUILayout.BeginHorizontal();
-        {
-            if (GUILayout.Button("Save"))
-            {
-                    
-            }
-        }
-        GUILayout.EndHorizontal();
+        //GUILayout.Label("No. of Segments");
+        segmentNum = EditorGUILayout.IntField("segements number", 0);
+        Show();
+     
+        EditorUtility.SetDirty(currentInstance);
     }
 
-    void CreatePattern()
+    void Show()
     {
-        // Create ?
+        for (int i = 0; i < currentInstance.Count; i++)
+        {
+            GUILayout.BeginVertical();
+            {
+                var segTemp = currentInstance[i];
+                for (int j = 0; j < segTemp.Count; j++)
+                {
+                    var tileTemp = segTemp[j];
+                    GUILayout.BeginHorizontal();
+                    {
+                        GUILayout.Label(tileTemp.name);
+                    }
+                    GUILayout.EndHorizontal();
+                }
+            }
+            GUILayout.EndVertical();
+        }
 
+    }
+
+    void Edit()
+    {
+        //for (int i = 0; i < segmentNum; i++)
+        //{
+        //    if (GUI.changed)
+        //    {
+        //        if (currentInstance.Count > segmentNum)
+        //        {
+        //            currentInstance.segmentList.RemoveRange(segmentNum-1, currentInstance.Count-segmentNum );
+        //        }
+        //        else
+        //        {
+        //            for (int i = 0; i < currentInstance.Count - segmentNum; i++)
+        //            {
+        //                currentInstance.segmentList.AddRange(currentInstance.Count, )
+        //            }
+        //        }
+        //    }
+        //}
     }
 }
--- Assets/Michael/Scripts/PatternSO.cs
+++ Assets/Michael/Scripts/PatternSO.cs
@@ -3,13 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-[CreateAssetMenu(fileName = "Pattern" , menuName = "Config/Pattern Editor/Pattern")]
+[CreateAssetMenu(fileName = "Pattern", menuName = "Config/Pattern Editor/Pattern")]
 public class PatternSO : ScriptableObject
 {
     public int difficulty;
     public float length;
     public float sectionLength;
-   //public List<ObjectSO>;
-    [SerializeField]
+    //public List<ObjectSO>;
+
     public List<Segment> segmentList = new List<Segment>();
+
+    public Segment this[int index]
+    {
+        get
+        {
+            return segmentList[index];
+        }
+        set
+        {
+            segmentList[index] = value;
+        }
+    }
+    public int Count
+    {
+        get
+        {
+            return segmentList.Count;
+        }
+    }
+
 }
