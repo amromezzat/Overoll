7502f79d30b56a1a40071c5e470df5725db4f78e
Michael Salama <MichaelSalama@users.noreply.github.com>
2018-05-16 12:07:50 +0200

    for some fix

--- Assets/Michael/Editor/PatternEditor.cs
+++ Assets/Michael/Editor/PatternEditor.cs
@@ -7,45 +7,27 @@ using UnityEditor;
 /// This script is used to modify obstacles in patterns segement by segment
 /// </summary>
 
-//[CustomEditor(typeof(Pattern))]
-//[CustomEditor(typeof(CoinGenerator))]
+[CustomEditor(typeof(PatternSO))]
 public class PatternEditor : Editor
 {
-    public int noOfSegments;
 
     public override void OnInspectorGUI()
     {
-        // call some function here to genereate <noOfSegments> segment(s)
+        DrawDefaultInspector();
+
         GUILayout.BeginHorizontal();
-        if (GUILayout.Button("far left lane"))
         {
-            // call the functuin of far left lane instantiation
+            if (GUILayout.Button("Save"))
+            {
+
+            }
         }
-
-        if (GUILayout.Button("left lane"))
-        {
-            // call the functuin of left lane instantiation
-
-        }
-
-        if (GUILayout.Button("Middle Lane"))
-        {
-            // call the functuin of middle instantiation
-
-        }
-
-        if (GUILayout.Button("Right Lane"))
-        {
-            // call the functuin of Right lane instantiation
-
-        }
-
-        if (GUILayout.Button("far Righy Lane"))
-        {
-            // call the functuin of far Right lane instantiation
-
-        }
-
         GUILayout.EndHorizontal();
+    }
+
+    void CreatePattern()
+    {
+        // Create ?
+
     }
 }
--- Assets/Michael/Scripts/PatternSO.cs
+++ Assets/Michael/Scripts/PatternSO.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+[CreateAssetMenu(fileName = "Pattern" , menuName = "Config/Pattern Editor/Pattern")]
 public class PatternSO : ScriptableObject
 {
     public int difficulty;
     public float length;
     public float sectionLength;
    //public List<ObjectSO>;
+    [SerializeField]
+    public List<Segment> segmentList = new List<Segment>();
 }
