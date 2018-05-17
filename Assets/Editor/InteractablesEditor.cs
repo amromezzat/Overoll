cf42ad72bbe16d1e0e6ed57eac95bb90a82d1acc
AUEzzat <amr.m.ezzat@gmail.com>
2018-05-17 15:42:12 +0200

    interactables editor and name list

---
+++ Assets/Editor/InteractablesEditor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(InteractablesDatabase))]
+public class InteractablesEditor : Editor
+{
+    InteractablesDatabase interactablesDB;
+    bool ShowInteractablesNames = true;
+
+    private void OnEnable()
+    {
+        interactablesDB = (InteractablesDatabase)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        ShowInteractablesNames = EditorGUILayout.Foldout(ShowInteractablesNames, "Interactables Names");
+        if (ShowInteractablesNames)
+        {
+            EditorGUI.BeginDisabledGroup(true);
+            EditorGUI.indentLevel++;
+            for (int i = 0; i < interactablesDB.interactablesNames.Count; i++)
+            {
+                EditorGUILayout.LabelField(interactablesDB.interactablesNames[i]);
+            }
+            EditorGUI.indentLevel--;
+            EditorGUI.EndDisabledGroup();
+        }
+
+        if (GUI.changed)
+        {
+            interactablesDB.interactablesNames = new List<string>(interactablesDB.Count);
+            for(int i = 0; i < interactablesDB.Count; i++)
+            {
+                interactablesDB.interactablesNames.Add(interactablesDB[i].name);
+            }
+        }
+    }
+}
