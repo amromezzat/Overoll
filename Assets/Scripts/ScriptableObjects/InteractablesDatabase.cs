08999ef0bc6ddd89373164d27476dc9726e4c7e9
AUEzzat <amr.m.ezzat@gmail.com>
2018-05-17 12:08:23 +0200

    class name change, interactable database

--- Assets/Scripts/ScriptableObjects/InteractablesDatabase.cs
+++ Assets/Scripts/ScriptableObjects/InteractablesDatabase.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 [CreateAssetMenu]
-public class ObstacleSystem : ScriptableObject {
+public class InteractableDatabase : ScriptableObject {
 
  public    List<TileType> Obstacles;
 
