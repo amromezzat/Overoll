d564e802d3e2ac47317279575138348d53c92cc0
tasnim000 <34035074+tasnim000@users.noreply.github.com>
2018-05-18 11:02:13 -0700

    Tile Generation first worked test

---
+++ Assets/Tasnim/scripts/Difficulty.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Difficulty {
+
+    public List<PatternSO> ListOfPatterns =new List<PatternSO>();
+    
+    public int Count
+    {
+        get
+        {
+          return  ListOfPatterns.Count;
+        }
+    }
+
+    public PatternSO this [int index]
+    {
+        get
+        {
+            return ListOfPatterns[index];
+        }
+        set
+        {
+            ListOfPatterns[index] = value;
+        }
+    }
+
+    public void RemoveAt(int index)
+    {
+        ListOfPatterns.RemoveAt(index);
+    }
+
+    public bool Contains(PatternSO pattern)
+    {
+        return ListOfPatterns.Contains(pattern);
+    }
+
+    public void Add(PatternSO pattern)
+    {
+        ListOfPatterns.Add(pattern);
+    }
+}
