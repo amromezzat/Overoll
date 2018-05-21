a170e98dc9c205c46646d11204cdfb90fdd58b55
tasnim <tassneemm0@gmail.com>
2018-05-21 14:48:01 +0200

    work health

---
+++ Assets/Scripts/MonoBehavior/Workers/BaseObstacle.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+interface Collidable
+{
+
+    void reactToCollision(int collidedHealth);
+    int Gethealth();
+}
+
+
+public class BaseObstacle :MonoBehaviour, Collidable {
+
+    public int obsHealth;
+    public Vector3 obsVelocity;
+    public WorkerHealth WH;
+    
+    enum obstacleState
+    {
+        Idle=1, // No one collide with it yet
+        Destroied=2, //the obstacle is destroid 
+        Broken=3  // lesa feh shewaia health metb2e
+    }
+
+    obstacleState state= obstacleState.Idle;
+
+
+    public void reactToCollision(int collidedHealth)
+    {
+        obsHealth = obsHealth - collidedHealth;
+
+
+
+        if (obsHealth <= 0)
+        {
+            state = obstacleState.Destroied;
+        }
+        else
+        {
+            state = obstacleState.Broken;
+        }
+        //-----------------------------------------
+
+       
+    }
+
+    public int Gethealth()
+    {
+      
+        return obsHealth;
+       
+    }
+    //-----------------------------------------------------------------------------------------
+  
+}
---
+++ Assets/Scripts/MonoBehavior/Workers/WorkerHealth.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorkerHealth : MonoBehaviour { 
+    enum workerState
+    {
+        Idle=1,
+        Dead=2,
+        Hit=3
+    }
+
+    workerState state = workerState.Idle;
+    public int workerHealth;
+    public int obsH;
+    public BaseObstacle baseObs;
+    public int CoinCount;
+
+    //------------------------------------------------
+
+    public void onTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "obstacle")
+        {
+            Collidable Other = other.GetComponent<Collidable>();
+
+            obsH = Other.Gethealth();
+            int wh = workerHealth;
+            workerHealth = workerHealth - obsH;
+
+           Other.reactToCollision(wh);
+        }
+
+        if (other.gameObject.tag == "coin")
+        {
+            Destroy(other);
+            //return to the coin pool
+            CoinCount += 1;
+            Debug.Log(CoinCount);
+        }
+
+
+    }
+
+
+
+}
