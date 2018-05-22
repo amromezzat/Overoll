089929ef89c2f546fe890a77402b7f699121ffa2
tasnim <tassneemm0@gmail.com>
2018-05-22 11:21:08 +0200

    Interactable Returner

---
+++ Assets/Scripts/MonoBehavior/Workers/CoinReturner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinReturner : MonoBehaviour {
+
+    public ObjectPool objPool;
+    public InteractablesDatabase iDB;
+    public PoolableType poolablename;
+    public GameState gState;
+   public void CoinToPool()
+    {
+        poolablename = iDB[gameObject.name];
+        objPool.ReturnToPool(poolablename, gameObject);
+    }
+    void Update()
+    {
+
+        if (gameObject.transform.position.z < gState.safeZone)
+        {
+            objPool.ReturnToPool(poolablename, gameObject);
+        }
+    }
+}
