32dec50ee9cb0dd8455b4773b003d7a2d9626e4c
Michael Salama <MichaelSalama@users.noreply.github.com>
2018-05-23 15:59:40 +0200

    Blank tile prefab changed

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -13,9 +13,17 @@ public class UIManager : MonoBehaviour
     public Animator settingAnim;
     public Animator storeAnim;
 
+    //public Canvas mainMenu
+
     public void PlayMoveBtn () {
         settingAnim.SetBool("SetBtnIsOut", false);
         storeAnim.SetBool("StoreBtnIsOut", false);
-    
+
+        
+    }
+
+    public void SettingsBtn()
+    {
+
     }
 }
