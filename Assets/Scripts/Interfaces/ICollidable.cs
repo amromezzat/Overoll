356f65196b1f21c38749257639ce1601342591ab
AUEzzat <amr.m.ezzat@gmail.com>
2018-05-24 14:26:55 +0200

    obstacles added

---
+++ Assets/Scripts/Interfaces/ICollidable.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+interface ICollidable
+{
+    void ReactToCollision(int collidedHealth);
+    int Gethealth();
+}
--- Assets/Scripts/MonoBehavior/Workers/WorkerHealth.cs
+++ Assets/Scripts/MonoBehavior/Workers/WorkerHealth.cs
@@ -3,37 +3,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WorkerHealth : MonoBehaviour { 
+public class WorkerHealth : MonoBehaviour
+{
     enum workerState
     {
-        Idle=1,
-        Dead=2,
-        Hit=3
+        Idle = 1,
+        Dead = 2,
+        Hit = 3
     }
 
     workerState state = workerState.Idle;
     public int workerHealth;
     public int obsH;
     public BaseObstacle baseObs;
- 
+
 
     //------------------------------------------------
 
     public void onTriggerEnter(Collider other)
     {
 
-        if (other.gameObject.tag == "obstacle")
+        if (other.gameObject.tag == "Obstacle")
         {
-            Collidable Other = other.GetComponent<Collidable>();
+            ICollidable Other = other.GetComponent<ICollidable>();
 
             obsH = Other.Gethealth();
             int wh = workerHealth;
             workerHealth = workerHealth - obsH;
 
-           Other.reactToCollision(wh);
+            Other.ReactToCollision(wh);
         }
 
-      
+
 
 
     }
