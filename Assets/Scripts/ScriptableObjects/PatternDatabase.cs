9b5adc89d74b8343741defb3f0eb85de28af72f9
AUEzzat <amr.m.ezzat@gmail.com>
2018-05-28 11:11:20 +0200

    mobile build compatability

--- Assets/Scripts/ScriptableObjects/PatternDatabase.cs
+++ Assets/Scripts/ScriptableObjects/PatternDatabase.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
 
 [CreateAssetMenu(fileName = "PatternDatabase", menuName = "Database/Pattern")]
 
