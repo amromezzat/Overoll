272a2a7f6871d8aafc5c47096f79fdad80b015e3
Michael Salama <MichaelSalama@users.noreply.github.com>
2018-06-05 17:11:08 +0200

    Audio Manager is here
    also a temp main theme audio is here

---
+++ Assets/Scripts/Sounds/Sound.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+
+    public AudioClip clip;
+
+    [Range (0,1)]
+    public float volume;
+    [Range (0.1f , 1)]
+    public float pitch;
+
+    public bool loop;
+
+    [HideInInspector]
+    public AudioSource source;
+}
