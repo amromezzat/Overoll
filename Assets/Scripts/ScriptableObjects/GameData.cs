1284dda688d92d7e1fc4efea4bf5a936159555bc
tasnim <tassneemm0@gmail.com>
2018-06-06 20:53:24 +0200

    prefabs

--- Assets/Scripts/ScriptableObjects/GameData.cs
+++ Assets/Scripts/ScriptableObjects/GameData.cs
@@ -6,7 +6,7 @@ using UnityEngine.Events;
 [CreateAssetMenu(fileName = "GameData", menuName = "Config/GameData")]
 public class GameData : ScriptableObject
 {
-    [HideInInspector]
+    
     public int difficulty;
     [HideInInspector]
     public int hrNum;
