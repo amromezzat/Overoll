a6dbae66180a9ffe5f5538233ed4cf21b3a7429c
AUEzzat <amr.m.ezzat@gmail.com>
2018-06-24 21:46:20 +0200

    fps limiter

---
+++ Assets/Scripts/MonoBehavior/Managers/FPSLimiter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class FPSLimiter : MonoBehaviour
+{
+    public int target = 30;
+
+    void Awake()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = target;
+    }
+
+    void Update()
+    {
+        if (Application.targetFrameRate != target)
+            Application.targetFrameRate = target;
+    }
+}
