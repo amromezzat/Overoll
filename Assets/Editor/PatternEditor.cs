8d032d26baca3892d1cba45b77a7daa593c284f8
tasnim <tassneemm0@gmail.com>
2018-06-24 19:17:33 +0200

    Some Patterns -_-

--- Assets/Editor/PatternEditor.cs
+++ Assets/Editor/PatternEditor.cs
@@ -43,7 +43,7 @@ public class PatternEditor : Editor
         EditablePattern();
         EditorGUILayout.Separator();
         GUILayout.Label("", GUI.skin.horizontalSlider);
-        RemoveAll();
+    //    RemoveAll();
 
         serializedObject.ApplyModifiedProperties();
         EditorUtility.SetDirty(pattern);
