b7801b254aef969ca7fd63b6452d02f6a95333e5
MichaelSalama <michael.fawzy91@yahoo.com>
2019-01-26 07:20:11 +0200

    Welcome Scene & Level Loader Bar

---
+++ Assets/Scripts/MonoBehavior/Managers/LevelLoader.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class LevelLoader : MonoBehaviour
+{
+    public GameObject loadingScreen;
+    public Slider slider;
+    public Text progressText;
+
+    private int sceneInd;
+
+    private void Start()
+    {
+        sceneInd = 1;
+        StartCoroutine(LoadAsynchronously(sceneInd));
+    }
+
+    //public void LoadLevel(int sceneIndex)
+    //{
+    //    StartCoroutine(LoadAsynchronously(sceneIndex));
+    //}
+
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            //Debug.Log(progress);
+            slider.value = progress;
+            progressText.text = progress * 100f + "%";
+            yield return null;
+        }
+    }
+}
