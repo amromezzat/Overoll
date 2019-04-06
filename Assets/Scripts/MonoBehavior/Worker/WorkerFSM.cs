e1be3133912bcdd86a036878fdf9930a0f3adf8f
MichaelSalama <michael.fawzy91@yahoo.com>
2019-04-06 19:26:53 +0200

    Mesh change at merge, Tools folder created

--- Assets/Scripts/MonoBehavior/Worker/WorkerFSM.cs
+++ Assets/Scripts/MonoBehavior/Worker/WorkerFSM.cs
@@ -72,18 +72,29 @@ public class WorkerFSM : MonoBehaviour, IHalt, ICollidable
     [HideInInspector]
     public List<Material> helmetsMaterial = new List<Material>();
 
-    [SerializeField]
-    List<SkinnedMeshRenderer> Helmets;
+    //[SerializeField]
+   // List<SkinnedMeshRenderer> Helmets;
 
     [SerializeField]
     Transform powerUpPosition;
 
+    [SerializeField]
+    public List<Mesh> helmetMeshes = new List<Mesh>();
+    [SerializeField]
+    public List<Mesh> overollMeshes = new List<Mesh>();
+
+    [SerializeField]
+    public SkinnedMeshRenderer helmet;
+
+    [SerializeField]
+    public SkinnedMeshRenderer overall;
+    
     private void Awake()
     {
-        foreach (SkinnedMeshRenderer helmet in Helmets)
-        {
-            helmetsMaterial.Add(helmet.material);
-        }
+        //foreach (SkinnedMeshRenderer helmet in Helmets)
+        //{
+        //    helmetsMaterial.Add(helmet.material);
+        //}
         mAnimator = GetComponent<Animator>();
         mCollider = GetComponent<BoxCollider>();
         workerReturner = GetComponent<WorkerReturner>();
@@ -117,6 +128,9 @@ public class WorkerFSM : MonoBehaviour, IHalt, ICollidable
             haltedState = WorkerState.Leader;
             currentState = WorkerState.Halted;
         }
+
+        helmet.sharedMesh = helmetMeshes[0];
+        overall.sharedMesh = overollMeshes[0];
     }
 
     private void OnDisable()
@@ -155,7 +169,7 @@ public class WorkerFSM : MonoBehaviour, IHalt, ICollidable
 
         positionWorker = new PositionWorker(wc, rb, transform, GetInstanceID());
         seekLeaderPosition = new SeekLeaderPosition(transform, wc, lanes);
-        mergerCollide = new MergerCollide(wc);
+        mergerCollide = new MergerCollide(wc, helmetMeshes, overollMeshes, this, helmet, overall);
         seekMasterMerger = new SeekMasterMerger(wc, rb, transform);
         positionMasterMerger = new PositionMasterMerger(wc, rb, transform, GetInstanceID());
 
