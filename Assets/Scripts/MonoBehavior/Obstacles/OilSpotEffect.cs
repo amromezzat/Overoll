022ac6aaf76dd7f580002be32774410ff4abe272
AUEzzat <amr.m.ezzat@gmail.com>
2019-05-08 02:26:11 +0200

    new oil added, oil play blob effect, new fog, jump and land animations replaced

---
+++ Assets/Scripts/MonoBehavior/Obstacles/OilSpotEffect.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OilSpotEffect : CollisionEffect
+{
+    [SerializeField]
+    ParticleSystem particleSystem;
+
+    public override void PlayEffect()
+    {
+        particleSystem.Play();
+    }
+}
